65cd2ed12618104f10ed2ac2f75cf403ee5fada0
SolalVall <solal.vallee@laposte.net>
2023-05-15 22:53:15 -0400

    (test)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "management_zone_v2_test-demo-tf" {
-  name        = "test-demo-tf"
+  name        = "test-demo"
   source      = "git::git@github.com:SolalVall/terraform-dynatrace-modules//management-zone?ref=main"
   description = "This terraform MZ is automatically created using Terraform"
   entity_selector_rules = [
