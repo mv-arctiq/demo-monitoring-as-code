3324ece9a82ff905b30edfe845f0d6ec8e252284
SolalVall <solal.vallee@laposte.net>
2023-05-19 12:23:42 -0400

    (demo)

--- terraform/production/slo.tf
+++ terraform/production/slo.tf
@@ -3,14 +3,14 @@ module "slo" {
   team_name = "my-team"
   slos = {
     "slo-demo-prod" = {
-      name           = "(prod)"
+      name             = "(prod)"
       description      = "This is a slo created with terraform for measuring application user experience for app with tag test"
-      warning        = 99.90
+      warning          = 99.90
       target           = 98.50
       timeframe        = "-1m"
       predefine_metric = "application_user_experience"
       filter_type      = "APPLICATION"
       filter_entity    = "My Web Application"
-   }
+    }
   }
 }
